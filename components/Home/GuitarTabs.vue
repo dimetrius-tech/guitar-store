6fe1be200cd9613ec690f1f65b8770aef35a9f3a
coding <coding.io@proton.me>
2025-01-08 16:18:48 +0200

    edit GuitarTabs.vue component

--- components/Home/GuitarTabs.vue
+++ components/Home/GuitarTabs.vue
@@ -1,32 +1,36 @@
 <script setup lang="ts">
+import {filename} from "pathe/utils";
+
 const activeTab = ref(null);
 
-const guitarImages = import.meta.glob('/assets/img/guitars', {eager: true});
-
+const glob = import.meta.glob('/assets/img/guitars/*', {eager: true});
+const images = Object.fromEntries(
+    Object.entries(glob).map(([key, value]) => [filename(key), value.default])
+)
 const guitars = [
   {
-    image: '@images/guitars/Admiral_Alba.png',
+    image: images['Admiral_Alba'],
     name: 'Admiral Alba',
     price: 500,
     inStock: true,
     stars: 5,
   },
   {
-    image: '@images/guitars/Fender_Strat_Ultra_MN.png',
+    image: images['Fender_Strat_Ultra_MN'],
     name: 'Fender Strat_Ultra_MN',
     price: 1200,
     inStock: true,
     stars: 5,
   },
   {
-    image: '@images/guitars/Cort_GB24jj.png',
+    image: images['Cort_GB24jj'],
     name: 'Cort_GB24jj (2 tone burst)',
     price: 800,
     inStock: true,
     stars: 5,
   },
   {
-    image: '@images/guitars/Cort_ad810e.png',
+    image: images['Cort_ad810e'],
     name: 'Cort_ad810e (white satin)',
     price: 800,
     isStock: true,
@@ -36,18 +40,24 @@ const guitars = [
 </script>
 
 <template>
-  <v-tabs v-model="activeTab" align-tabs="center">
-    <v-tab value="all">All guitars</v-tab>
-    <v-tab value="electric">Electric guitars</v-tab>
-    <v-tab value="bass">Bass guitars</v-tab>
-    <v-tab value="classical">Classical guitars</v-tab>
+  <v-tabs v-model="activeTab" align-tabs="center" class="border-b-2">
+    <v-tab value="all" :class="{'font-bold': activeTab === 'all'}">All guitars</v-tab>
+    <v-tab value="electric" :class="{'font-bold': activeTab === 'electric'}">Electric guitars</v-tab>
+    <v-tab value="bass" :class="{'font-bold': activeTab === 'bass'}">Bass guitars</v-tab>
+    <v-tab value="classical" :class="{'font-bold': activeTab === 'classical'}">Classical guitars</v-tab>
   </v-tabs>
   <v-tabs-window v-model="activeTab">
     <v-tabs-window-item value="all">
-      <v-card v-for="(guitar, index) in guitars" :key="index">
-        <v-img width="237" :src="guitar.image" />
-        <v-card-text>{{guitar.name}}</v-card-text>
-      </v-card>
+      <CardDesktop :cards="guitars" />
+    </v-tabs-window-item>
+    <v-tabs-window-item value="electric">
+      <CardDesktop :cards="guitars" />
+    </v-tabs-window-item>
+    <v-tabs-window-item value="bass">
+      <CardDesktop :cards="guitars" />
+    </v-tabs-window-item>
+    <v-tabs-window-item value="classical">
+      <CardDesktop :cards="guitars" />
     </v-tabs-window-item>
   </v-tabs-window>
 </template>
