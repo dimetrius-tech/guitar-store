96a4a2ffb52fc565111cc57763be90da31e87bc7
coding <coding.io@proton.me>
2025-01-08 16:27:43 +0200

    set up default styles for vuetify components

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -7,13 +7,15 @@ import 'vuetify/styles';
 const GuitarTheme: ThemeDefinition = {
     colors: {
         background: '#110E0E',
-        surface: '#110E0E'
+        surface: '#110E0E',
+        surfaceLight: '#FFFFFF',
+        additionalGray: '#585050',
     },
     variables: {
         h1FontSize: '54px',
         h2FontSize: '48px',
         h3FontSize: '42px',
-        fontFamilyHeading: '"Poppins", sans-serif',
+        fontFamily: '"Poppins", sans-serif',
     },
 }
 
@@ -27,6 +29,26 @@ export default defineNuxtPlugin((nuxtApp) => {
                 GuitarTheme
             }
         },
+        defaults: {
+            VFooter: {
+                style: [{borderTop: '1px solid white'}]
+            },
+            VMain: {
+                style: [{paddingBottom: '80px'}]
+            },
+            VTabs: {
+                style: [{borderBottom: '1px solid white'}, {marginBottom: '20px'}]
+            },
+            VCard: {
+                style: [{backgroundColor: '#000000'}, {padding: '20px'}]
+            },
+            VExpansionPanel: {
+                style: [{backgroundColor: '#000000'}, {marginBottom: '30px'}, {padding: '20px'}]
+            },
+            VBtn:{
+                style: [{backgroundColor: '#110E0E'}]
+            }
+        }
     });
 
     nuxtApp.vueApp.use(vuetify);
